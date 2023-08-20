72417f3e50999c0be5cda885b6e595200ea4375d
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2023-08-20 12:01:53 -0600

    fix test

--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, Edge, RisingEdge
 
 @cocotb.test()
 async def test_fsm(dut):
@@ -17,21 +17,24 @@ async def test_fsm(dut):
     # Enable
     dut._log.info("Enabling DUT")
     dut.ena.value = 1
-    await ClockCycles(dut.clk, 10)
     
     # Check state
     dut._log.info("Checking finite state machine")
-    for i in range(15):
-        await ClockCycles(dut.clk, 1)
-        if dut.tt_um_fsm.state_reg.value == 1:
-            assert int(dut.salida.value) == 10
-        elif dut.tt_um_fsm.state_reg.value == 2:
-            assert int(dut.salida.value) == 5
-        elif dut.tt_um_fsm.state_reg.value == 3:
-            assert int(dut.salida.value) == 15
-        elif dut.tt_um_fsm.state_reg.value == 0:
-            assert int(dut.salida.value) == 0
-
+    for i in range(20):
+        await RisingEdge(dut.clk) 
+        state = dut.tt_um_fsm.current_state.value
+        dut._log.info("State: %d", dut.tt_um_fsm.current_state.value)
+        await RisingEdge(dut.clk)
+        output = dut.salida.value
+        dut._log.info("Value: %d", dut.salida.value)
+        if state == 0:
+            assert output == 10
+        elif state == 1:
+            assert output == 5
+        elif state == 2:
+            assert output == 15
+        else:
+            assert output == 3
 
     
     
--- src/tt_um_fsm.v
+++ src/tt_um_fsm.v
@@ -13,82 +13,89 @@ module tt_um_fsm #( parameter MAX_COUNT = 24'd10_000_000 ) (
 
     wire reset = ! rst_n;
     reg [7:0] led_out;
+    reg [2:0] state_reg = 3'd0;
     assign uo_out[7:0] = led_out;
-    assign uio_out[7:0] = led_out;
     // use bidirectionals as outputs
     assign uio_oe = 8'b11111111;
-    reg [7:0] counter = 8'd0;
+    reg done;
+    reg [1:0] state;
 
-    // FSM states
-    localparam [2:0] S_IDLE = 3'b000;
-    localparam [2:0] S_COUNT = 3'b001;
-    localparam [2:0] S_WAIT  = 3'b010;
-    localparam [2:0] S_DONE  = 3'b011;
+    // Parámetros para los estados
+    localparam IDLE = 2'b00;
+    localparam COUNT = 2'b01;
+    localparam RESET = 2'b10;
 
-    // FSM state register
-    reg [2:0] state_reg = 3'd0;
-    // FSM next state logic
-    always @(posedge clk) begin
-        if (reset) begin
-            state_reg <= S_IDLE;
+    // Registros de estado y próximo estado
+    reg [1:0] current_state, next_state;
+
+    assign uio_out[7:0] = current_state;
+
+    // Contador
+    reg [3:0] counter;
+
+    // Lógica de la máquina de estados
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            current_state <= IDLE;
+            counter <= 4'b0000;
+            done <= 1'b0;
         end else begin
-            case (state_reg)
-                S_IDLE: begin
-                    if (ena) begin
-                        state_reg <= S_COUNT;
-                    end else begin
-                        state_reg <= S_IDLE;
-                    end
-                end
-                S_COUNT: begin
-                    if (counter == 3'd3) begin
-                        state_reg <= S_WAIT;
-                    end else begin
-                        state_reg <= S_COUNT;
-                    end
-                end
-                S_WAIT: begin
-                    if (ena) begin
-                        state_reg <= S_DONE;
-                    end else begin
-                        state_reg <= S_WAIT;
-                    end
-                end
-                S_DONE: begin
-                    if (ena) begin
-                        state_reg <= S_IDLE;
-                    end else begin
-                        state_reg <= S_DONE;
-                    end
-                end
-                default: begin
-                    state_reg <= S_IDLE;
-                end
-            endcase
+            current_state <= next_state;
         end
     end
 
-    // FSM output logic
-    always @(posedge clk) begin
-        case (state_reg)
-            S_IDLE: begin
-                counter <= 0;
-                led_out = 8'd0;
+    always @(*) begin
+        case (current_state)
+            IDLE: begin
+                if (ena) begin
+                    next_state = COUNT;
+                end else begin
+                    next_state = IDLE;
+                end
             end
-            S_COUNT: begin
-                counter <= counter + 1;
-                led_out = 8'd10;
+            COUNT: begin
+                if (counter == 4'b0011) begin
+                    next_state = RESET;
+                end else begin
+                    next_state = COUNT;
+                end
             end
-            S_WAIT: begin
-                led_out = 8'd5;
-            end
-            S_DONE: begin
-                led_out = 8'd15;
+            RESET: begin
+                next_state = IDLE;
             end
             default: begin
-                led_out = 8'd17;
+                next_state = IDLE;
             end
         endcase
+    end
+
+    // Salidas basadas en el estado
+    always @(posedge clk) begin
+        case (current_state)
+            IDLE: begin
+                done <= 1'b0;
+                led_out <= 8'd10;
+            end
+            COUNT: begin
+                counter <= counter + 1;
+                done <= 1'b0;
+                led_out <= 8'd5;
+            end
+            RESET: begin
+                counter <= 4'b0000;
+                done <= 1'b1;
+                led_out <= 8'd15;
+            end
+            default: begin
+                done <= 1'b0;
+                led_out <= 8'd3;
+            end
+        endcase
+    end
+
+    // Asignar el estado actual a la salida
+    always @(*) begin
+        state = current_state;
     end
 
 endmodule
