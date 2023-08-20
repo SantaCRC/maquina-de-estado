610e5a7ea54ea014aed445db1466341f5fc59891
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2023-08-19 20:15:40 -0600

    fix 1

--- src/tt_um_fsm.v
+++ src/tt_um_fsm.v
@@ -26,7 +26,6 @@ module tt_um_fsm #( parameter MAX_COUNT = 24'd10_000_000 ) (
 
     // FSM state register
     reg [2:0] state_reg;
-    assign uio_out[7:0] = state_reg;
     // FSM next state logic
     always @(posedge clk or negedge reset) begin
         if (reset) begin
